77b7bf1ebecb269862172e980811a079dcbd5b75
oceancat11 <160452107+oceancat11@users.noreply.github.com>
2024-02-21 09:26:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ first_time=1
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
-  
+
 @app.route("/main",methods=["GET","POST"])
 def main():
- global r,first_time
-  if first_time==1:
   r=request.form.get("r")
-     first_time=0
- return(render_template("main.html",r=r))
+  return(render_template("main.html",r=r))
 
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
- return(render_template("image_gpt.html"))
+  return(render_template("image_gpt.html"))
 
 @app.route("/end",methods=["GET","POST"])
 def end():
-  
- return(render_template("end.html"))
+  first_time=1
+  return(render_template("end.html"))
 
-if __name__== "_main_":
-  app.run()
+  if __name__=="__main__":
+         app.run()
