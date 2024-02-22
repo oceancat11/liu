b9ff6d2c6840439c08c3c32580d8451966548fdd
oceancat11 <160452107+oceancat11@users.noreply.github.com>
2024-02-22 12:07:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask,request,render_template
 import replicate
 import os
 import time
-from openai import OpenAI
 
-openai_api_key=os.getenv('OPENAI_API_KEY')
 os.environ["REPLICATE_API_TOKEN"]="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"
-model = OpenAI(api_key=openai_api_key)
 
 app = Flask(__name__)
 
@@ -32,17 +29,8 @@ def text_gpt():
 @app.route("/text_result",methods=["GET","POST"])
 def text_result():
     q = request.form.get("q")
-    r = model.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "user",
-             "content": q
-        }
-     ]
- )
-    time.sleep(5)
-    return(render_template("text_result.html",r=r.choices[0].message.content))
+    return(render_template("text_result.html",r="API not ready"))
+
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
@@ -67,5 +55,4 @@ def end():
 
 if __name__ == "__main__":
     app.run()
-
  
