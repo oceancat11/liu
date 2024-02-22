327c0fd29f524053eff948df5eb38fa1d543c92a
oceancat11 <160452107+oceancat11@users.noreply.github.com>
2024-02-22 12:00:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,17 @@ def text_gpt():
 @app.route("/text_result",methods=["GET","POST"])
 def text_result():
     q = request.form.get("q")
-    return(render_template("text_result.html",r="API not ready"))
-
+    r = model.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user",
+             "content": q
+        }
+     ]
+ )
+    time.sleep(5)
+    return(render_template("text_result.html",r=r.choices[0].message.content))
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
